1109a2be111a0c4ecd6462033faf887338c405ff
mzohaibm <zohaibattari106@gmail.com>
2024-03-29 09:00:43 -0700

    first commit

---
+++ src/main/Card.java
@@ -0,0 +1,19 @@
+package main;
+
+public class Card
+{
+    int value;
+    String suit;
+
+    int getValue()
+    {
+        //placeholder
+        return 0;
+    }
+
+    String getSuit()
+    {
+        //placeholder
+        return suit;
+    }
+}
---
+++ src/main/Hand.java
@@ -0,0 +1,30 @@
+package main;
+
+import java.util.Vector;
+
+public class Hand
+{
+
+    /*same as in the deck class for cards, see for reference on it*/
+    Vector<Integer> cards = new Vector<Integer>();
+
+    void hand()
+    {
+
+    }
+
+    void addCard()
+    {
+
+    }
+
+    void removeCard()
+    {
+
+    }
+
+    void display()
+    {
+
+    }
+}
